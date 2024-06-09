92cad4ad3e325e73ab35b608395e13b9363b3005
HamzaImran54 <hamza.imran@softwarealliance.dk>
2024-06-09 18:58:42 +0500

    Creating Build

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output : 'export',
-  // experimental: {
-  //   typedRoutes: true,
-  //   instrumentationHook: true,
-  // },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "images.pexels.com",
-  //       port: "",
-  //       pathname: "/**",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "images.unsplash.com",
-  //       port: "",
-  //       pathname: "/**",
-  //     },
-  //   ],
-  // },
+  experimental: {
+    typedRoutes: true,
+    instrumentationHook: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
